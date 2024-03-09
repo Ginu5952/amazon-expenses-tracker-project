86c7c833d70964810cc2bf11b5156c2c8b5a4752
Ginu <ginujestin@gmail.com>
2024-03-09 17:15:53 +0100

    completed

--- amazon_expenses_tracker.py
+++ amazon_expenses_tracker.py
@@ -3,19 +3,38 @@ import time
 from datetime import datetime
 import sys
 
+# Colors
+
+RESET = '\033[0m'
+RED = '\033[91m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+BLUE = '\033[94m'
+PURPLE = '\033[95m'
+CYAN = '\033[96m'
+
+# Font Styles
+
+BOLD = '\033[1m'
+ITALIC = '\033[3m'
+UNDERLINE = '\033[4m'
+
+
 # Global variables
 
-user_name = input('Enter your name: ')
+user_name = input(YELLOW + ITALIC + '\nEnter your name: ' + RESET)
 user_number = 0
 choice = 0
 purchase_details = dict()
 temporary_list = list()
+
 
 # Validating password
 
 def validate_credentials(pswd):
 
     validation_pattern = r'^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?!.*[:_-])(?!.*\s).{8,}$'
+
     if re.match(validation_pattern,pswd):
         return True
     else:
@@ -23,23 +42,24 @@ def validate_credentials(pswd):
 
     
 def valid_password():
-    attempt = 0
+
+    password_attempt = 0
     while True:
         print('\n')
-        password = input('Enter the password: ')
+        password = input(YELLOW + ITALIC + 'Enter the password: ' + RESET)
 
         if validate_credentials(password):
             return password
         else:
-            attempt += 1
+            password_attempt += 1
             print('\n')
-            print('Invalid password! Please Try again with valid password\n') 
-            if attempt == 3:
+            print(RED + BOLD + 'Invalid password! Please Try again with valid password\n' + RESET) 
+            if password_attempt == 3:
                 print('\n')
-                print("You've used all attempts. Please wait for 5 seconds.........")
+                print(RED + BOLD + "You've used all attempts. Please wait for 5 seconds........." + RESET)
                 time.sleep(5)
-            elif attempt > 3:
-                print('Please register again.......')
+            elif password_attempt > 3:
+                print(RED + 'Please register again.......' + RESET)
                 print('\n')
                 break
 
@@ -47,7 +67,7 @@ def valid_password():
 
 def validate_mobile_number(mobile_number):
 
-    mob_validation_pattern = r'^\+49'
+    mob_validation_pattern = r'^\+49\d{11}$'
 
     if re.search(mob_validation_pattern,mobile_number):
         
@@ -60,14 +80,14 @@ validPassword = valid_password()
 def check_mobile_number():
     while True:
         print('\n')
-        user_mobile_number = input('Please enter phone number to continue: ') 
+        user_mobile_number = input(GREEN + ITALIC + 'Please enter phone number to continue: ' + RESET) 
         
         if validate_mobile_number(user_mobile_number):
             
             return user_mobile_number
         else:
             print('\n')
-            print('invalid country code.') 
+            print(RED + 'invalid country code or it should be an 11 digit number.' + RESET) 
 
 # Menu Choices
             
@@ -78,7 +98,7 @@ def choose(choices):
             
             if saved_details is not None and len(saved_details) == 5:
                 print('\n')
-                print('Purchase saved*******')
+                print(GREEN + 'Purchase saved*******' + RESET)
                 temporary_list.append([saved_details.copy()])
                 menu()
             else:
@@ -87,7 +107,7 @@ def choose(choices):
                    
     elif choices == '2':
         print('\n')
-        print('Generating report...')
+        print(GREEN + 'Generating report...' + RESET)
         print('...')
         time.sleep(2)
         
@@ -99,13 +119,27 @@ def choose(choices):
             menu()   
 
     elif choices == '3':
-        print('\nQuitting program...')  
-        time.sleep(1)
+        
         print('\n')
-        print(f'Thank you for your visit, {user_name.title()}. Goodbye!\n') 
-        sys.exit()
+        user_input = input(RED + BOLD + "Are you sure you want to quit? [Y/N]: " + RESET).strip().lower()
+    
+        if user_input == 'y':
+            print(RED + ITALIC + '\nQuitting program...' + RESET)  
+            time.sleep(1)
+            print('\n')
+            print(BLUE + BOLD + f'Thank you for your visit, {user_name.title()}. Goodbye!\n' + RESET) 
+            for i in range(len(user_name.title())):
+                print(GREEN + BOLD + " " * i + user_name[i] + RESET)
+            sys.exit()
+        elif user_input == 'n':
+            print('\n')
+            print(GREEN + BOLD + "Cancelled. Continuing..." + RESET)
+            menu()
+        else:
+            print("Invalid input. Please enter 'Y' or 'N'.")
+        
     else:
-        print('invalid choice press a valid choice')  
+        print(RED + ITALIC + 'invalid choice press a valid choice' + RESET)  
         menu()
            
 
@@ -114,7 +148,6 @@ def choose(choices):
 def validate_date(datee):
 
     date_pattern = r'^(0[1-9]|1[0-2])[-/](0[1-9]|[12][0-9]|3[01])[-/]\d{4}$'
-
     if re.search(date_pattern,datee):
         modified = re.sub(r'-', '/', datee)
         purchase_details['date'] = modified
@@ -165,22 +198,22 @@ def enter_your_choice():
        
        while True:
         print('\n')
-        date = input('Enter the date of the purchase (MM/DD/YYYY) or (MM-DD-YYYY): ')  
+        date = input(YELLOW + ITALIC + 'Enter the date of the purchase (MM/DD/YYYY) or (MM-DD-YYYY): ' + RESET)  
         
         if validate_date(date):
             while True:
-                item = input('Enter the item purchased: ')    
+                item = input(YELLOW + ITALIC + 'Enter the item purchased: ' + RESET)    
                 if validate_item(item):
                     while True:
-                        cost_str = input('Enter the cost of the item in Euro: ')
+                        cost_str = input(YELLOW + ITALIC + 'Enter the cost of the item in Euro: ' + RESET)
                         cost = validate_cost(cost_str)
                         if cost is not None:
                             while True:
-                                weightt = input('Enter the weight of the item in kg: ')
+                                weightt = input(YELLOW + ITALIC + 'Enter the weight of the item in kg: ' + RESET)
                                 weight = validate_weight(weightt)
                                 if weight is not None:
                                     while True:
-                                        quantityy = input('Enter the quantity purchased: ')
+                                        quantityy = input(YELLOW + ITALIC + 'Enter the quantity purchased: ' + RESET)
                                         quantity = validate_quantity(quantityy)
                                         
                                         if quantity is not None:
@@ -195,7 +228,7 @@ def enter_your_choice():
                                     print('Please enter a valid weight...')
                 else:
                     print('\n')
-                    print('Item should be at least 3 characters.')    
+                    print(RED + BOLD + 'Item should be at least 3 characters.' + RESET)    
         else:
             print('\n')
             print('Invalid format! Please Try again with valid format')
@@ -248,50 +281,50 @@ def generate_amazon_report():
     output = calculations_amazon_report(temporary_list)
     number = mask_mobile_number(user_number)
     today_date = datetime.today().strftime("%m/%d/%Y")
-    print('\n\t\t\t-------------------------')
-    print('\t\t\t| Amazon Expense Report |')
-    print('\t\t\t-------------------------')
+    print(PURPLE + BOLD + '\n\t\t\t-------------------------' + RESET)
+    print(PURPLE + BOLD + '\t\t\t| Amazon Expense Report |' + RESET)
+    print(PURPLE + BOLD + '\t\t\t-------------------------' + RESET)
     print('\n')
-    print(f'name: {user_name.title()} \t password: **** \t Tel: {number} \t Date:{today_date}')
-    print('--------------------------------------------------------------------------------')
-    print('DELIVERY CHARGES','TOTAL ITEM COST',sep='\t')
-    print(f'{round(output[2])} EURO\t\t\t{round(output[3])} EURO\n')
-    print('MOST EXPENSIVE','LEAST EXPENSIVE',sep='\t\t')
-    print(f'name: {output[0]}\t\tname: {output[1]}')
-    print(f'cost: {round(output[4])} EURO\t\tcost: {round(output[5])} EURO\n')
-    print(f'AVERAGE COST OF ITEM PER ORDER: {round(output[6])} EURO')
-    print(f'PURCHASE DATE RANGE: {output[7]}')
-    print('--------')
+    print(BLUE + ITALIC + f'name: {user_name.title()} \t password: **** \t Tel: {number} \t Date:{today_date}' + RESET)
+    print(PURPLE + BOLD + '--------------------------------------------------------------------------------' + RESET)
+    print(BLUE + ITALIC + 'DELIVERY CHARGES',BLUE + ITALIC + 'TOTAL ITEM COST',sep='\t' + RESET)
+    print(PURPLE + BOLD + f'{round(output[2])} EURO\t\t\t{round(output[3])} EURO\n' + RESET)
+    print(BLUE + ITALIC + 'MOST EXPENSIVE',BLUE + ITALIC + 'LEAST EXPENSIVE',sep='\t\t' + RESET)
+    print(BLUE + BOLD + f'name: {output[0]}\t\tname: {output[1]}' + RESET)
+    print(BLUE + BOLD + f'cost: {round(output[4])} EURO\t\tcost: {round(output[5])} EURO\n' + RESET)
+    print(GREEN + ITALIC + f'AVERAGE COST OF ITEM PER ORDER: {round(output[6])} EURO' + RESET)
+    print(GREEN + ITALIC + f'PURCHASE DATE RANGE: {output[7]}' + RESET)
+    print(PURPLE + BOLD + '---------------------------------' + RESET)
     if output[3] > 500 :
-        print('Note: You have  exceeded the spending limit of 500 EURO')
+        print(RED + BOLD + 'Note: You have  exceeded the spending limit of 500 EURO' + RESET)
     else:
-        print('Note: You have not exceeded the spending limit of 500 EURO')  
+        print(GREEN + ITALIC + 'Note: You have not exceeded the spending limit of 500 EURO' + RESET)  
 
  # Creating Menu  
                  
 def menu():
         print('\n')
-        print('What would you like to do?')
-        print('1. Enter a purchase')
-        print('2. Generate a report')
-        print('3. Quit')
-        choice = input('Enter your choice (1/2/3):')
-        print('...')
+        print(YELLOW + ITALIC + 'What would you like to do?' + RESET)
+        print(PURPLE + ITALIC + '1. Enter a purchase' + RESET)
+        print(GREEN + ITALIC + '2. Generate a report' + RESET)
+        print(RED + BOLD + '3. Quit' + RESET)
+        print('\n')
+        choice = input(YELLOW + ITALIC + 'Enter your choice (1/2/3): ' + RESET)
         choose(choice)            
 
 if validPassword:
 
     print('\n')
-    print('\t\tRegistration successful!') 
-    print('\t\t=======================')
-    print('\t\t| Welcome to Amazon! |')
-    print('\t\t***********************')  
+    print(PURPLE + '\t\tRegistration successful!' + RESET)  
+    print(BLUE + '\t\t=======================' + RESET)
+    print(PURPLE + '\t\t| Welcome to Amazon! |' + RESET)
+    print(BLUE + '\t\t***********************' + RESET)  
     checkMobileNumber = check_mobile_number()  
     user_number = checkMobileNumber
 
     if checkMobileNumber:
         print('\n')
-        print(f'Hello, {user_name.title()}! Welcome to the Amazon Expense Tracker!')
+        print(YELLOW + BOLD + f'Hello, {user_name.title()}! Welcome to the Amazon Expense Tracker!' + RESET)
         menu()
     else:
         print('Unsuccessful')
@@ -321,4 +354,4 @@ else:
 
 
 
-    
+       
